10837e2211916a82ba89b622f07f11ccec2af118
Hegong Li <787132415@qq.com>
2017-02-20 14:21:16 +0800

    导航
    导航

---
+++ HeaderAnimationEffects/MYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYViewController.h
+//  HeaderAnimationEffects
+//
+//  Created by XSD on 2017/2/17.
+//  Copyright © 2017年 Ryan_Man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYViewController : UIViewController
+
+@end
---
+++ HeaderAnimationEffects/MYViewController.m
@@ -0,0 +1,28 @@
+//
+//  MYViewController.m
+//  HeaderAnimationEffects
+//
+//  Created by XSD on 2017/2/17.
+//  Copyright © 2017年 Ryan_Man. All rights reserved.
+//
+
+#import "MYViewController.h"
+
+@interface MYViewController ()
+
+@end
+
+@implementation MYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    self.title = @"s首页";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
